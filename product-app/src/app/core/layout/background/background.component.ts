842f8161955a6448c7a140596c03779f77662908
Dylan Adal <dylanadal.00@gmail.com>
2025-04-27 16:55:21 -0400

    goback button

--- product-app/src/app/core/layout/background/background.component.ts
+++ product-app/src/app/core/layout/background/background.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { GoBackComponent } from './goback/goback.component';
 
 @Component({
   selector: 'app-background',
+  imports: [GoBackComponent],
   templateUrl: './background.component.html',
   styleUrl: './background.component.css',
   standalone: true,
-  imports: [],
 })
 export class BackgroundComponent {}
