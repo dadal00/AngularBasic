769a917c5002507d8058945bf94e272df8c1e43e
Dylan Adal <dylanadal.00@gmail.com>
2025-04-28 18:14:37 -0400

    product card details

--- src/app/features/products/components/product-detail/product-detail.component.ts
+++ src/app/features/products/components/product-detail/product-detail.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../models/product.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'product-detail',
   imports: [],
   templateUrl: './product-detail.component.html',
-  styleUrl: './product-detail.component.css'
+  styleUrls: [
+    './product-detail.component.css',
+    '../../styles/products-shared.css'
+  ]
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit {
+  id!: string;
+  product: Product | undefined;
+  private subscription: Subscription = new Subscription();
 
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService,
+  ) {}
+
+  ngOnInit(): void {
+    this.id = String(this.route.snapshot.paramMap.get('id'));
+    this.subscription.add(
+      this.productService.getProducts().subscribe((data) => {
+        this.product = data.find(p => p.id === this.id);
+      }),
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
