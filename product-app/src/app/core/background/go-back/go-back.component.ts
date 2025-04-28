92cc62d80ce67b9dbf817cb07000042b86ba95b6
Dylan Adal <dylanadal.00@gmail.com>
2025-04-27 21:53:28 -0400

    back nav

--- product-app/src/app/core/background/go-back/go-back.component.ts
+++ product-app/src/app/core/background/go-back/go-back.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'goback-button',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './go-back.component.css',
   standalone: true,
 })
-export class GoBackComponent {}
+export class GoBackComponent {
+  currentPath = '';
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(() => {
+      this.currentPath = this.router.url;
+    });
+  }
+
+  goBack() {
+    if (this.currentPath == '/') {
+      return;
+    }
+    let pathParts = this.currentPath.split('/').filter(Boolean);
+    pathParts.pop();
+    const parentPath = '/' + pathParts.join('/');
+    this.router.navigate([parentPath]);
+  }
+}
