b301d98baff01f0ac1808bf104a5038b035a998d
Dylan Adal <dylanadal.00@gmail.com>
2025-04-27 21:53:10 -0400

    basic nav to products list

--- product-app/src/app/app.routes.ts
+++ product-app/src/app/app.routes.ts
@@ -3,12 +3,16 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () =>
-      import('./core/background/background.component'),
+    loadComponent: () => import('./core/background/background.component'),
     children: [
       {
         path: '',
         loadComponent: () => import('./features/home/home.component'),
+      },
+      {
+        path: 'products',
+        loadComponent: () =>
+          import('./features/products/product-list/product-list.component'),
       },
     ],
   },
--- product-app/src/app/features/home/home.component.ts
+++ product-app/src/app/features/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,4 +7,10 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export default class HomeComponent {}
+export default class HomeComponent {
+  constructor(private router: Router) {}
+
+  toProductList() {
+    this.router.navigate(['/products']);
+  }
+}
